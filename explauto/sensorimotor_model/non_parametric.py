f97339cd681f4c50dc3737c66da889d391fb2b30
catherinehenry <catherinemshenry@gmail.com>
2024-01-22 14:32:11 -0700

    adjust config for LWLR-NONE to k = 8

--- explauto/sensorimotor_model/non_parametric.py
+++ explauto/sensorimotor_model/non_parametric.py
@@ -105,7 +105,7 @@ sensorimotor_models = {
     # BFGS -> https://en.wikipedia.org/wiki/Broyden%E2%80%93Fletcher%E2%80%93Goldfarb%E2%80%93Shanno_algorithm
     # 'LWLR-BFGS': (NonParametric, {'default': {'fwd': 'LWLR', 'k':10, 'sigma':0.1, 'inv': 'L-BFGS-B', 'maxfun':50}}),
     # CMAES -> Covariance Matrix Adaptation Evolution Strategy
-    'LWLR-NONE': (NonParametric, {'default': {'fwd': 'LWLR', 'k':15, 'sigma':0.1, 'inv': 'NONE'}}),
+    'LWLR-NONE': (NonParametric, {'default': {'fwd': 'LWLR', 'k':8, 'sigma':0.1, 'inv': 'NONE'}}),
     'NSLWLR-NONE': (NonParametric, {'default': {'fwd': 'LWLR', 'k':8, 'sigma':0.1, 'inv': 'NONE'}}),
     'NSLWLR-CMAES': (NonParametric, {'default': {'fwd': 'NSLWLR', 'k':10, 'sigma':0.1, 'inv': 'CMAES', 'cmaes_sigma':0.05, 'maxfevals':20}}),
 
