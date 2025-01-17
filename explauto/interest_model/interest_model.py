1c730ed1431bc6e5e20a027f9be05f56397aaf34
catherinehenry <catherinemshenry@gmail.com>
2025-01-17 14:34:37 -0700

    support kwargs so I can pass robot_world

--- explauto/interest_model/interest_model.py
+++ explauto/interest_model/interest_model.py
@@ -6,10 +6,11 @@ class InterestModel(object, metaclass=ABCMeta):
     def __init__(self, expl_dims):
         self.expl_dims = expl_dims
 
+    # take kwargs so that any kw args not in the config (so, non static) can be passed
     @classmethod
-    def from_configuration(cls, conf, expl_dims, im_name, config_name='default'):
+    def from_configuration(cls, conf, expl_dims, im_name, config_name='default', **kwargs):
         im_cls, im_configs = interest_models[im_name]
-        return im_cls(conf, expl_dims, **im_configs[config_name])
+        return im_cls(conf, expl_dims, **im_configs[config_name], **kwargs)
 
     @abstractmethod
     def sample(self):
