990e4a826254fb03e04c2934984c843530046b07
catherinehenry <catherinemshenry@gmail.com>
2026-03-23 18:34:27 -0600

    change logging around different competence evaluations

--- explauto/interest_model/tree.py
+++ explauto/interest_model/tree.py
@@ -191,8 +191,11 @@ class InterestTree(InterestModel, Observable):
             self.data_x = np.append(self.data_x, np.array([xy[self.expl_dims]]), axis=0)
         if self.competence_measure is prediction_error_cos_dist_exp:
             cos_sim, cos_dist, competence = self.competence_measure(xy, ms, self.bounds)
-            self.emit(f"[{flow_uuid}] competence", f"[cos sim: {cos_sim}, cos dist: {cos_dist}] bounded cos distance between target and reached: {competence}")
-        elif self.competence_measure is competence_exp or self.competence_measure is competence_cos_dist_exp:
+            self.emit(f"[{flow_uuid}] competence", f"[cos sim: {round(cos_sim, 4)}, cos dist: {round(cos_dist, 4)}] bounded cos distance between target and reached: {competence}")
+        elif  self.competence_measure is competence_cos_dist_exp:
+            cos_sim, cos_dist, competence = self.competence_measure(xy, ms, self.bounds)
+            self.emit(f"[{flow_uuid}] competence", f"[cos sim: {round(cos_sim, 4)}, cos dist: {round(cos_dist, 4)}], competence {competence}")
+        elif self.competence_measure is competence_exp:
             competence = self.competence_measure(xy, ms, self.bounds)
             self.emit(f"[{flow_uuid}] competence", f"competence {competence}")
         if self.data_c is None:
